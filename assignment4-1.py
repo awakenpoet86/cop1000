afee8044599feea2536e0bd5514d9e96b4a7177a
Niki <awakenpoet86@gmail.com>
2025-03-11 15:23:48 -0400

    commit

--- assignment4-1.py
+++ assignment4-1.py
@@ -9,17 +9,17 @@ charge = 35.0
 # Number of characters.
 numChars = 8
 # Color of characters.
-color = "Gold"
+color = "gold"
 # Type of wood.
-woodType = "Oak"
+woodType = "oak"
 # Write assignment and if statements here as appropriate.
 if numChars <=5:
     charge += 0
 if numChars >5:
     charge += (numChars-5) * 4
-if color =="Gold":
+if color.upper() =="GOLD":
     charge += 15
-if woodType == "Oak":
+if woodType.upper() == "OAK":
     charge += 20
 # Output Charge for this sign.
 print("The charge for this sign is $" + str(charge))
