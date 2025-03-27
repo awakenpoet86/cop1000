f7122302efc07b6ecfae05a82a0b1a92c5ae42d9
Niki <awakenpoet86@gmail.com>
2025-03-27 14:56:14 -0400

    commit

---
+++ 6-1/assignment6-1.py
@@ -0,0 +1,22 @@
+AllowedVehiclesList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
+print("*"*32)
+print("AutoCountry Vehicle Finder v0.1")
+print("*"*32)
+print("Please Enter the following number below from the following menu:")
+print("")
+print("1. PRINT all Authorized Vehicles")
+print("2. Exit")
+choice = int(input())
+while choice !=2:
+    print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicle:")
+    for car in AllowedVehiclesList:
+        print(car)
+    print("*"*32)
+    print("AutoCountry Vehicle Finder v0.1")
+    print("*"*32)
+    print("Please Enter the following number below from the following menu:")
+    print("")
+    print("1. PRINT all Authorized Vehicles")
+    print("2. Exit")
+    choice = int(input())
+print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
